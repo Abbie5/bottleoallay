aefc1dd1ec98e48c8b41042e254f45d3cf480ef8
Abbie <abigail@ient.me>
2022-08-03 23:20:04 +0100

    Use glass bottle instead of bottle o enchanting as the visual item

--- src/main/java/cc/abbie/bottleoallay/BottleOAllay.java
+++ src/main/java/cc/abbie/bottleoallay/BottleOAllay.java
@@ -1,14 +1,9 @@
 package cc.abbie.bottleoallay;
 
-import eu.pb4.polymer.api.item.PolymerSpawnEggItem;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 
 public class BottleOAllay implements ModInitializer {
     public static final Item BOTTLE_O_ALLAY = new BottleOAllayItem();
--- src/main/java/cc/abbie/bottleoallay/BottleOAllayItem.java
+++ src/main/java/cc/abbie/bottleoallay/BottleOAllayItem.java
@@ -1,25 +1,22 @@
 package cc.abbie.bottleoallay;
 
+import eu.pb4.polymer.api.item.PolymerItemUtils;
 import eu.pb4.polymer.api.item.PolymerSpawnEggItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.world.InteractionHand;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 public class BottleOAllayItem extends PolymerSpawnEggItem {
     public BottleOAllayItem() {
         super(
                 EntityType.ALLAY,
-                Items.EXPERIENCE_BOTTLE,
+                Items.GLASS_BOTTLE,
                 new FabricItemSettings().group(CreativeModeTab.TAB_MISC).stacksTo(1)
         );
     }
@@ -32,4 +29,12 @@ public class BottleOAllayItem extends PolymerSpawnEggItem {
         }
         return result;
     }
+
+    @Override
+    public ItemStack getPolymerItemStack(ItemStack itemStack, ServerPlayer player) {
+        ItemStack out = PolymerItemUtils.createItemStack(itemStack, player);
+        out.enchant(Enchantments.FISHING_SPEED, 0);
+        return out;
+    }
+
 }
